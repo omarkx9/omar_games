8c1e0f03d259b9c554088e6d46187ccc1193c8aa
omarkx9 <omarkx9@gmail.com>
2026-07-10 21:01:57 +0300

    remove if and else

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -3,8 +3,8 @@ print("Welcome to the hardest game in the world")
 computer_choice = random.randint(1,10)
 user_choice = int(input("Enter a number betwen (1 and 10): "))
 lives = 3
-if user_choice >=1 and user_choice <=10:
-    while user_choice != computer_choice and lives > 0:
+
+    while user_choice != computer_choice and lives > 0 and user_choice >=1 and user_choice <=10:
         if user_choice < computer_choice:
             print("higher")
         else:
@@ -14,7 +14,7 @@ if user_choice >=1 and user_choice <=10:
         lives -= 1
     if lives == 0 and user_choice != computer_choice:
         print(f"Sorry you lose! the computer chose was {computer_choice}")
+    elif user_choice >10 or user_choice < 0:
+        print("Invaild choice..... \nplease Enter a number betwen (1 and 10) ")
     else:
         print(f"Congraulition you won!\nthe computer chose was {computer_choice}")
-else:
-    print("Invaild choice..... \nplease Enter a number betwen (1 and 10) ")
