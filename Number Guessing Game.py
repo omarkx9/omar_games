c04a966e3c4981dbe750ddcba832cff70c1ec651
omarkx9 <omarkx9@gmail.com>
2026-07-10 21:03:01 +0300

    remove tab

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -4,17 +4,17 @@ computer_choice = random.randint(1,10)
 user_choice = int(input("Enter a number betwen (1 and 10): "))
 lives = 3
 
-    while user_choice != computer_choice and lives > 0 and user_choice >=1 and user_choice <=10:
-        if user_choice < computer_choice:
-            print("higher")
-        else:
-            print("lower")
-        print(f"you have {lives} more tries")
-        user_choice = int(input("Enter a number betwen (1 and 10): "))
-        lives -= 1
-    if lives == 0 and user_choice != computer_choice:
-        print(f"Sorry you lose! the computer chose was {computer_choice}")
-    elif user_choice >10 or user_choice < 0:
-        print("Invaild choice..... \nplease Enter a number betwen (1 and 10) ")
+while user_choice != computer_choice and lives > 0 and user_choice >=1 and user_choice <=10:
+    if user_choice < computer_choice:
+        print("higher")
     else:
-        print(f"Congraulition you won!\nthe computer chose was {computer_choice}")
+        print("lower")
+    print(f"you have {lives} more tries")
+    user_choice = int(input("Enter a number betwen (1 and 10): "))
+    lives -= 1
+if lives == 0 and user_choice != computer_choice:
+    print(f"Sorry you lose! the computer chose was {computer_choice}")
+elif user_choice >10 or user_choice < 0:
+    print("Invaild choice..... \nplease Enter a number betwen (1 and 10) ")
+else:
+    print(f"Congraulition you won!\nthe computer chose was {computer_choice}")
