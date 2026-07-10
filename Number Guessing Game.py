82ebfdc93defa34a6ac4655182db4227d72d87f0
omarkx9 <omarkx9@gmail.com>
2026-07-10 21:23:40 +0300

    add tries

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -3,17 +3,19 @@ print("🎲-------Welcome to Guessing Game-------🎲")
 computer_choice = random.randint(1,10)
 user_choice = int(input("Enter a number betwen (1 and 10): "))
 lives = 6
+tries = 1
 while user_choice != computer_choice and lives > 0 and user_choice >=1 and user_choice <=10:
+    tries += 1
     lives -= 1
     print(f"you have {lives} more tries")
     if user_choice > computer_choice:
-        print("⬇️ Too high! Try again ⬇️")
+        print("⬇️  Too high! Try again ⬇️")
     else:
-        print("⬆️ Too low! Try again ⬆️")
+        print("⬆️  Too low! Try again ⬆️")
     user_choice = int(input("Enter a number betwen (1 and 10): "))
 if lives == 0 and user_choice != computer_choice:
     print(f"Sorry you lose! the computer chose was {computer_choice}")
 elif user_choice >10 or user_choice < 0:
     print("Invaild choice..... \nplease Enter a number betwen (1 and 10) ")
 else:
-    print(f"💯Congraulition you won!\nthe computer chose was {computer_choice}")
+    print(f"💯Congraulition you won!\nYou guessed the number in {tries} tries")
