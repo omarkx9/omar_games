7fbd067583c69f686a014c1c153ef02274fde73b
omarkx9 <omarkx9@gmail.com>
2026-07-13 02:53:04 +0300

    new option

--- to_do_list.py
+++ to_do_list.py
@@ -1,34 +1,50 @@
-print("📑-------welcome to the To-Do List App!-------📑\n")
+print("\n\t 📑-------welcome to the To-Do List App!-------📑")
 tasks = []
 while True:
     print("""
+    _________________
     Choose an option:
     1. Add task
     2. View tasks
     3. Delete task
     4. Exit
+    -----------------
     """)
     choice = int(input("Enter your choice (1-4): "))
     if choice < 5 and choice > 0:
         if choice == 1:
             task = input("Enter the task: ")
             tasks.append(task)
-            print(f"👌 Task ({task}) added!\n")
+            print(f"👌 Task ({task}) added!")
         elif choice == 2:
-            i = 0
-            print("📃 Your tasks 📋:")
-            for x in tasks:
-                i += 1
-                print(i,".",x)
+            if not tasks:
+                print("📪 No tasks Yet")
+            else:
+                i = 0
+                print("📃 Your tasks 📋:")
+                for x in tasks:
+                    i += 1
+                    print(i,".",x)
         elif choice == 3:
-            i = 0
-            print("📃 Your tasks 📋:")
-            for x in tasks:
-                i += 1
-                print(i,".",x)
-            task_number = int(input("Enter task number to delet: "))
-            del_tasks = tasks.pop(task_number-1)
-            print(f"🗑️ Deleted: {del_tasks}")
+            if not tasks:
+                print("📪 No tasks Yet")
+            else:
+                i = 0
+                print("📃 Your tasks 📋:")
+                for x in tasks:
+                    i += 1
+                    print(i,".",x)
+                task_number = int(input("Enter task number to delet: "))
+                if task_number <= len(tasks) and task_number > 0:
+                    del_tasks = tasks.pop(task_number-1)
+                    print(f"🗑️ Deleted: {del_tasks}")
+                    i = 0
+                    print("📃 Your tasks 📋:")
+                    for x in tasks:
+                        i += 1
+                        print(i,".",x)
+                else:
+                    print("😐  😑")
         else:
             print("👋 Good bye!")
             break
