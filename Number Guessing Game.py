e8bd6ca27b49a2d9a2688aa8f15cab3e77e8edaf
omarkx9 <omarkx9@gmail.com>
2026-07-15 13:34:02 +0300

    new vireable

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,22 +1,22 @@
 import random
 print("🎲-------Welcome to Guessing Game-------🎲")
 computer_number = random.randint(1,10)
-tries = 0
-attempts = 5
+attempets = 0
+tries = 5
 while True:
-    if attempts != 0:
-            tries += 1
-            print(f"you have {attempts} more tries")
+    if tries != 0:
+            attempets += 1
+            print(f"you have {tries} more tries")
             user_number = int(input("Enter a number betwen (1 and 10): "))
             if user_number < 11 and user_number > 0:
                 if user_number == computer_number:
-                    print(f"💯Congratulations you won!\nYou guess the number in {tries} treis\n\tthe nubmer was ({computer_number})")
+                    print(f"💯Congratulations you won!\nYou guess the number in {attempets} treis\n\tthe nubmer was ({computer_number})")
                     break
                 elif user_number > computer_number:
                     print("⬇️  Too high! Try again ⬇️")
                 elif user_number < computer_number:
                     print("⬆️  Too low! Try again ⬆️")
-                attempts -= 1
+                tries -= 1
             else:
                 print("Invaild choice..... Try again ")
                 continue
