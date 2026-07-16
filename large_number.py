f72a5a0656676130441fccbee35859e20be4b349
omarkx9 <omarkx9@gmail.com>
2026-07-16 16:26:03 +0300

    the new code

--- large_number.py
+++ large_number.py
@@ -1,7 +1,9 @@
 no1 = float(input("Please Enter the first number: "))
 no2 = float(input("Please Enter the second number: "))
 no3 = float(input("Please Enter the third number: "))
-if no1 >= no2 and no1 >= no3:
+if no1 == no2 and no1 == no3:
+    print(f"All numbers is {no1}")
+elif no1 >= no2 and no1 >= no3:
     print(f"The largest number is {no1}")
 elif no2 >= no1 and no2 >= no3:
     print(f"The largest number is {no2}")
