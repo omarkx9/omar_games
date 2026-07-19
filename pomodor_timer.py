0933437134686fccd709b2358461d7ab73cef03c
omarkx9 <omarkx9@gmail.com>
2026-07-19 08:17:23 +0300

    new emojis

--- pomodor_timer.py
+++ pomodor_timer.py
@@ -1,15 +1,13 @@
 import time
 print("⌛-------Welcome to the Pomodoro Timer!-------⌛")
-mint = int(input("Enter time in minutes: "))
+mint = int(input("\t🕰️ Enter time in minutes: "))
 total = mint * 60
-while total > 0:
+while total > -1:
     mintues = total // 60
     sec = total % 60
     total -= 1
     clock = f"{mintues:02d}:{sec:02d}"
-    print(f"\r⏱️ Time remaining: {clock}",end="")
+    print(f"\r\t⏱️ Time remaining: {clock}",end="")
     time.sleep(1)
 print()
-time.sleep
-print("⏱️ Time remaining: 00:00")
-print("Times up , Take a break")
+print("\t🛑Times up , Take a break")
