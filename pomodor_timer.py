377cdc44ba698dda8179f37f70e58a940185a429
omarkx9 <omarkx9@gmail.com>
2026-07-19 14:11:22 +0300

    if number zero

--- pomodor_timer.py
+++ pomodor_timer.py
@@ -1,13 +1,16 @@
 import time
 print("⌛-------Welcome to the Pomodoro Timer!-------⌛")
-mint = int(input("\t🕰️ Enter time in minutes: "))
-total = mint * 60
-while total > -1:
-    mintues = total // 60
-    sec = total % 60
-    total -= 1
-    clock = f"{mintues:02d}:{sec:02d}"
-    print(f"\r\t⏱️ Time remaining: {clock}",end="")
-    time.sleep(1)
-print()
-print("\t🛑Times up , Take a break")
+number_of_seconde = int(input("\t🕰️ Enter time in minutes: "))
+total = number_of_seconde * 60
+if number_of_seconde != 0:
+    while total > -1:
+        mintues = total // 60
+        sec = total % 60
+        total -= 1
+        clock = f"{mintues:02d}:{sec:02d}"
+        print(f"\r\t⏱️ Time remaining: {clock}",end="")
+        time.sleep(1)
+    print()
+    print("\t🛑Times up , Take a break")
+else:
+    print("😧 What!!!!!!! these number is zero 😧")
