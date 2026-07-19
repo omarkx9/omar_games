d82776e53437e580855a2d15fd62ef83c16e723c
omarkx9 <omarkx9@gmail.com>
2026-07-19 14:18:06 +0300

    >0

--- pomodor_timer.py
+++ pomodor_timer.py
@@ -2,7 +2,7 @@ import time
 print("⌛-------Welcome to the Pomodoro Timer!-------⌛")
 number_of_seconde = int(input("\t🕰️ Enter time in minutes: "))
 total = number_of_seconde * 60
-if number_of_seconde != 0:
+if number_of_seconde > 0:
     while total > -1:
         mintues = total // 60
         sec = total % 60
