383516a3eb2e95e8a116c7feba56264bab6ba4bc
omarkx9 <omarkx9@gmail.com>
2026-07-22 18:17:13 +0300

    new opition

--- Positive_ Negative_Zero.py
+++ Positive_ Negative_Zero.py
@@ -1,16 +1,30 @@
 print("\n\t-------Welcome to Positive and Negative and Zero number-------\n")
-positive_number = []
-negative_number = []
-zero_number = []
-str_number = input("Enter  numbers to sveral: ").split()
-number = []
+str_number = input("Enter numbers separated by space: ").split()
+numbers = []
 for x in str_number:
-    number.append(int(x))
-for y in number:
-    if y < 0:
-        negative_number.append(y)
-    elif y == 0:
-        zero_number.append(y)
+    numbers.append(int(x))
+negative = []
+positive = []
+zero = []
+for i in numbers:
+    if i < 0:
+        negative.append(i)
+    elif i > 0:
+        positive.append(i)
     else:
-        positive_number.append(y)
-print(f"\n\t\tPpsitive number: {len(positive_number)}\n\t\tNegative number: {len(negative_number)}\n\t\tZero number: {len(zero_number)}\n")
+        zero.append(i)
+print(f"\nzero : {len(zero)} ")
+print(f"negative : {len(negative)}")
+print(f"positive : {len(positive)}")
+while True:
+    see = input("\nDo you want to see numbers? (yes or no): ").lower()
+    if see == "yes":
+        print(f"\n\t\tPositive number: {positive}")
+        print(f"\t\tNegative number: {negative}")
+        print(f"\t\tZero number: {zero}\n")
+        break
+    elif see == "no":
+        print(f"\nGood bye 👋\n")
+        break
+    else:
+        print(f"\nInvalid input.......... ({see}) is not yes or no Try again\n")
