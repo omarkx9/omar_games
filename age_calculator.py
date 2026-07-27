e9240c852f81cf4ab81aba7e33fd70c1ddf8718e
omarkx9 <omarkx9@gmail.com>
2026-07-28 01:07:26 +0300

    you can try again

--- age_calculator.py
+++ age_calculator.py
@@ -1,12 +1,23 @@
 print("\n\t----Age in Days and Month and Hours Calculator----")
 print()
-years = int(input("\t\t   ~~~Enter your age in years~~~: "))
-print(f"\t\t   !!!You Was Born In {2026 - years}!!!")
-month = years * 12
-days = years * 365
-hours = years * 8760
-mintutes = years * 525600
-seconde = years * 31622400 
-print("\t\t+++++++++++++++++++++++++++++++")
-print(f"\t\t|& You Lived For {days:,} Days  &|\n\t\t|& And {hours:,} Hours          &|\n\t\t|& And {mintutes:,} Mintutes    &|\n\t\t|& And {seconde:,} Seconde    &|")
-print("\t\t+++++++++++++++++++++++++++++++")
+while True:
+    years = input("\t\t   ~~~Enter your age in years~~~: ").strip()
+    if years.isdigit():
+        years = int(years)
+        print(f"\t\t   !!!You Was Born In {2026 - years}!!!")
+        month = years * 12
+        days = years * 365
+        hours = years * 8760
+        mintutes = years * 525600
+        seconde = years * 31622400 
+        print("\t\t+++++++++++++++++++++++++++++++")
+        print(f"\t\t|& You Lived For {days:,} Days  &|\n\t\t|& And {hours:,} Hours          &|\n\t\t|& And {mintutes:,} Mintutes    &|\n\t\t|& And {seconde:,} Seconde    &|")
+        print("\t\t+++++++++++++++++++++++++++++++")
+        again = input("Enter (\"Yes\") If you want to enter a nother number or (Enter): ").lower().strip()
+        if not again:
+            print("Good bye")
+            break
+        else:
+            continue
+    else:
+        print(f"{years} is not a number please Try again.....")
