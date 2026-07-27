160132a4657b32ba5b068edf4f127bd8457d1b1a
omarkx9 <omarkx9@gmail.com>
2026-07-28 00:53:59 +0300

    add seocnde

--- age_calculator.py
+++ age_calculator.py
@@ -1,6 +1,12 @@
-print("---Age in Days and Month and Hours Calculator---")
-years = int(input("Enter your age in years: "))
+print("\n\t----Age in Days and Month and Hours Calculator----")
+print()
+years = int(input("\t\t   ~~~Enter your age in years~~~: "))
+print(f"\t\t   !!!You Was Born In {2026 - years}!!!")
 month = years * 12
 days = years * 365
 hours = years * 8760
-print(f"you have lived for {days} days\nand {month} month \nand {hours} hours")
+mintutes = years * 525600
+seconde = years * 31622400 
+print("\t\t+++++++++++++++++++++++++++++++")
+print(f"\t\t|& You Lived For {days:,} Days  &|\n\t\t|& And {hours:,} Hours          &|\n\t\t|& And {mintutes:,} Mintutes    &|\n\t\t|& And {seconde:,} Seconde    &|")
+print("\t\t+++++++++++++++++++++++++++++++")
