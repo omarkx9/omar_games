7690e2dbcb832e52734b379f74290e8c6e42ec3b
omarkx9 <omarkx9@gmail.com>
2026-07-28 20:02:23 +0300

    senior

--- Even_or_Odd.py
+++ Even_or_Odd.py
@@ -2,6 +2,7 @@
 print("\n\t-------Welcome to Even or Odd-------")
 # ask user to enter a number and save the number in a varibale
 number = int(input('Enter a number: '))
+# jonior
 # check if the number is Even
 if number % 2 == 0:
     # print to the user his number and the number is Even
@@ -10,3 +11,5 @@ if number % 2 == 0:
 else:
     #print to the user his nubmer and the number is Odd
     print(f"{number} These number is Odd")
+# senior
+print(f"{number} These Number is Even" if number % 2 == 0 else f"{number} These Number is Odd")
