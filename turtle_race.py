f4c7698f24a1aa58a7a6ad6d1d01dc7af4060115
omarkx9 <omarkx9@gmail.com>
2026-08-10 21:36:14 +0300

    invalid choice

--- turtle_race.py
+++ turtle_race.py
@@ -2,93 +2,92 @@ from turtle import Turtle,Screen
 import random
 import time
 
+window = Screen()
+window.title("Omar Turtle Race 🐢🏁")
+window.setup(800,400)
+
+
 while True:
-    window = Screen()
-    window.setup(1000,1000)
-    window.title("Omar Turtle Race 🐢🏁")
+    window.clear()
     window.bgcolor("gray")
+    y = Turtle("turtle")
+    y.hideturtle()
+    y.color("white")
+    y.write("😑GOOOOO!!😑",align="center",font=("arial",50))
+
     user_choice = window.textinput("!Guess the Winner! 🏆","Type a color: Red, Blue or Green? 🎨")
+    words = ["أحمر","احمر","أزرق","ازرق","أخضر","اخضر","red","blue","green"]
+    if user_choice in words:
+        WY = [-70,0,70]
+        colors = ["red","blue","green"]
+        turtles = []
 
-    words = ['red','blue','green',"احمر","ازرق","اخضر"]
+        for x in range(3):
+            turtle = Turtle("turtle")
+            turtles.append(turtle)
+            turtles[x].color(colors[x])
+            turtles[x].penup()
+            turtles[x].goto(-280,WY[x])
 
-    sam = Turtle("turtle")
-    tom = Turtle("turtle")
-    omar = Turtle("turtle")
+        turtles[0].color("black")
+        turtles[0].pensize(8)
+        turtles[0].goto(280,0)
+        turtles[0].pendown()
+        turtles[0].left(90)
+        turtles[0].forward(200)
+        turtles[0].left(180)
+        turtles[0].forward(800)
+        turtles[0].penup()
+        turtles[0].color("red")
+        turtles[0].goto(-280,-70)
+        turtles[0].left(90)
 
-    if user_choice.lower() in words:
-
-        sam.color("black","white")
-        sam.penup()
-        sam.pensize(5)
-        sam.goto(480,500)
-        sam.pendown()
-        sam.left(90)
-        sam.forward(500)
-        sam.left(180)
-        sam.forward(2000)
-        sam.left(90)
-
-        sam.color("green")
-        tom.color("blue")
-        omar.color("red")
-
-        sam.speed("slowest")
-        tom.speed("slowest")
-        omar.speed("slowest")
-
-        sam.penup()
-        tom.penup()
-        omar.penup()
-
-        sam.goto(-480,250)
-        tom.goto(-480,0)
-        omar.goto(-480,-250)
-
-        def race(turtle1,turtle2,turtle3):  
-            turtle1.speed(random.choice(("fastest","fast","slow","slowest")))
-            turtle2.speed(random.choice(("fastest","fast","slow","slowest")))
-            turtle3.speed(random.choice(("fastest","fast","slow","slowest")))
-            while True:
-                turtle1.forward(random.randint(1,8))
-                
-                turtle2.forward(random.randint(1,8))
-                
-                turtle3.forward(random.randint(1,8))
-
-                if turtle3.xcor() >= 480:
-                    return ("red","احمر","أحمر")
-                elif turtle2.xcor() >= 480:
-                    return ("blue","ازرق","أزرق")
-                elif turtle1.xcor() >= 480:
-                    return ("green","اخضر", "أخضر")
-
-        def win(user_choice,result):
-            if user_choice.lower() in result:
-                window.bgcolor("green")
-                x = Turtle("turtle")
-                x.hideturtle()
-                x.penup()
-                x.color("white")
-                x.write("You Win! 🎉🏆",align="center",font=("arial",80,"bold"))
+        def turn_to_english(word):
+            if word == "احمر" or word == "أحمر":
+                return "red"
+            elif word == "ازرق" or word == "أزرق":
+                return "blue"
+            elif word == "اخضر" or word == "أخضر":
+                return "green"
             else:
-                window.bgcolor("red")
-                x = Turtle("turtle")
-                x.hideturtle()
-                x.penup()
-                x.color("white")
-                x.write(f"You Lose! 😢\n{result[0]} Wins! 🏆",align="center",font=("arial",80,"bold"))
-    
-            time.sleep(3)
-            
+                return word
 
-        result = race(sam,tom,omar)
+        def rice():
+            game_on = True
+            while game_on:
+                for x in turtles:
+                    if x.xcor() >= 280:
+                        game_on = False
+                        display_result(turn_to_english(user_choice) == x.color()[0],turn_to_english(x.color()[0]))
+                    else:
+                        x.forward(random.randint(1,5))
 
-        win(user_choice,result)
-        
+        def display_result(user,wineer):
+            window.clear()
+            window.bgcolor("red")
+            result = Turtle()
+            result.hideturtle()
+            result.penup()
+            result.color("white")
+            if user:
+                window.bgcolor("green")
+                result.write(f"You Chose {turn_to_english(user_choice)}\nYou Win😎",align="center",font=("arial",20))
+            else:
+                result.write(f"You Chose {turn_to_english(user_choice)}\nYou Lose😑\nThe Wineer Was {wineer}",align="center",font=("arial",20))
+            time.sleep(5)
+
+
+        rice()
+
+        sam = Turtle()
         window.clear()
+        window.bgcolor("gray")
+        sam.hideturtle()
+        sam.color("dark gray")
+        sam.goto(0,0)
+        sam.write("Press any key to Exit... 👋",align="center",font = ("arial",30))
         break
 
-        
     else:
         window.clear()
         window.bgcolor("gray")
@@ -96,15 +95,6 @@ while True:
         y.hideturtle()
         y.color("white")
         y.write("Invalid Choice... 😑❌",align="center",font=("arial",50))
-        time.sleep(5)
+        time.sleep(2)
         continue
-
-
-window.clear()
-window.bgcolor("gray")
-sam.hideturtle()
-sam.color("dark gray")
-sam.goto(0,0)
-sam.write("Press any key to Exit... 👋",align="center",font = ("arial",50))
-
 window.exitonclick()
